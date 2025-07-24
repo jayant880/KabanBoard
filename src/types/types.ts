320f6fd38dbba22d15030bf0a1421c81590beb40
Jayant <188503548+jayant880@users.noreply.github.com>
2025-07-24 23:34:03 +0000

    Refactor: changed interface type and data structure

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,23 +1,23 @@
-export type Priority = 'High' | 'Medium' | 'Low' | 'None'
+export type Priority = 'High' | 'Medium' | 'Low' | 'None';
 
 export interface Task {
-  id: string
-  title: string
-  description: string | null
-  isCompleted: boolean
-  priority: Priority
-  dueDate: Date | null
-  createdAt: Date | null
+  id: string;
+  title: string;
+  description: string | null;
+  isCompleted: boolean;
+  priority: Priority;
+  dueDate: Date | null;
+  createdAt: Date | null;
 }
 
 export interface Column {
-  id: string
-  name: string
-  taskList: Task[]
+  id: string;
+  name: string;
+  taskIds: string[];
 }
 
 export interface Board {
-  id: string
-  name: string
-  columnList: Column[]
+  id: string;
+  name: string;
+  columnOrder: string[];
 }
